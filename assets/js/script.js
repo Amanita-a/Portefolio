73013d677251515a8e55e9a115d6453df9223016
Ama <258918420+Amanita-a@users.noreply.github.com>
2026-03-08 13:41:27 +0100

    page projet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-    const aPropos = document.getElementById("pageAPropos");
-    const boutonAPropos = document.getElementById("boutonAPropos");
-    const croixAPropos = document.getElementById("croixAPropos");
+const aPropos = document.getElementById("pageAPropos");
+const boutonAPropos = document.getElementById("boutonAPropos");
+const croixAPropos = document.getElementById("croixAPropos");
 
-    const projet = document.getElementById("pageProjet");
-    const boutonProjet = document.getElementById("boutonProjet");
-    const croixProjet = document.getElementById("croixProjet");
+const projet = document.getElementById("pageProjet");
+const boutonProjet = document.getElementById("boutonProjet");
+const croixProjet = document.getElementById("croixProjet");
 
-    const contact = document.getElementById("pageContact");
-    const boutonContact = document.getElementById("boutonContact");
-    const croixContact = document.getElementById("croixContact")
-
-
+const contact = document.getElementById("pageContact");
+const boutonContact = document.getElementById("boutonContact");
+const croixContact = document.getElementById("croixContact")
 
 
 
 
+
+// Page A Propos Apparition et dispartion
 boutonAPropos.addEventListener("click", () =>{
     aPropos.classList.add("afficher");
 });
@@ -22,21 +22,28 @@ croixAPropos.addEventListener("click", () =>{
     aPropos.classList.remove("afficher");
 });
 document.addEventListener("click", (event) => {
-
     if (!aPropos.contains(event.target) && 
         !boutonAPropos.contains(event.target)) {
-
         aPropos.classList.remove("afficher");
     }
-
 });
-
+// 
+// Page Projet Apparition et disparition
 boutonProjet.onclick = function() {
     projet.classList.add("afficher");
 }
 croixProjet.onclick = function() {
     projet.classList.remove("afficher");
 }
+document.addEventListener("click", (event) => {
+    if (!projet.contains(event.target) && 
+        !boutonProjet.contains(event.target)) {
+        projet.classList.remove("afficher");
+    }
+});
+
+
+
 boutonContact.addEventListener("click", () =>{
     contact.classList.add("afficher");
 });
